9c7e809d2c5f86fd7bd3f00a9c959349865c988b
Pedro Alves <pta2002@pta2002.com>
2021-12-07 21:25:02 +0000

    Usar JavaFX

--- DSS/src/main/java/dss/Main.java
+++ DSS/src/main/java/dss/Main.java
@@ -1,7 +1,20 @@
 package dss;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    // Estamos a usar JavaFX, portanto este é o método principal da aplicação
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dss/gui/Main.fxml"));
+        Scene mainScene = new Scene(loader.load());
+
+        stage.setScene(mainScene);
+        stage.setTitle("Sistema de Gestão de Reparações");
+
+        stage.show();
     }
 }
---
+++ DSS/src/main/java/dss/gui/Main.java
@@ -0,0 +1,16 @@
+package dss.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Main {
+    @FXML
+    private TextField nif;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginButton;
+}
